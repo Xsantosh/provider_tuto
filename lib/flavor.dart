43df89fc5f828e055007148b7cb6b0708c1586b0
santosh <xsantosh7@gmail.com>
2021-03-19 13:45:46 +0530

    added flavor

---
+++ lib/flavor.dart
@@ -0,0 +1,6 @@
+///Run with:
+///flutter run --flavor dev -t lib/main.dart
+///flutter run --flavor staging -t lib/main.dart
+///flutter run --flavor prod -t lib/main.dart
+///NOTE: On IOS, we need to create matching Xcode
+enum Flavor { dev, staging, prod }
