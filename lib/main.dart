7684cf2b97186ae581bd2f459a4f8307fa7f8471
santosh <xsantosh7@gmail.com>
2021-03-19 14:15:03 +0530

    added provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_tuto/flavor.dart';
 import 'package:provider_tuto/my_app.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Provider<Flavor>.value(
+    value: Flavor.dev,
+    child: MyApp(),
+  ));
 }
